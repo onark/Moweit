d2d37f712edfe846543950f95fad32581452c4f0
Hene <henri.bergman@gmail.com>
2013-09-02 19:45:37 +0300

    Created new coming soon page

--- src/com/moweit/home/HomeServlet.java
+++ src/com/moweit/home/HomeServlet.java
@@ -1,13 +1,36 @@
 package com.moweit.home;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class HomeServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>MOWE-IT - MOBILE, WEB, IT-SOLUTIONS");
+		out.println("</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("");
+		out.println("Hello World");
+		out.println("");
+		out.println("");
+		out.println("");
+		out.println("");
+		out.println("");
+		out.println("");
+		out.println("");
+		out.println("</body>");
+		out.println("</html>");
+		
 	}
 }
